9fc36a663a90c748b5f9753fc225d95cc22d72e9
brettmt10 <brettmt10@gmail.com>
2025-10-15 19:29:42 -0400

    enable CORS

--- src/server/main.py
+++ src/server/main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500"
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Entry(BaseModel):
     name: str
     accuracy: float
+    color: str
 
 @app.post("/max-accuracy")
-async def max_engine(entry: Entry):
-    content = entry.model_dump()    
-    return content
+async def max_engine(entry: list[Entry]):
+    return entry
