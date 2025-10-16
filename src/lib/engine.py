7ade11497a554914537cce4eb6c6648d7d9eed4e
brettmt10 <brettmt10@gmail.com>
2025-10-16 16:58:40 -0400

    robust infer function w specialized field

--- src/lib/engine.py
+++ src/lib/engine.py
@@ -4,6 +4,6 @@ import numpy as np
 def infer(df: pd.DataFrame):
     o_na = df['accuracy'].isna()
     df['accuracy'] = pd.to_numeric(df['accuracy'], errors='coerce')
-    n_coerce = df['accuracy'].isna() & ~o_na
-    df.loc[n_coerce, 'accuracy'] = np.inf
+    df['inferred_coerce'] = df['accuracy'].isna() & ~o_na
+    df.loc[df['inferred_coerce'], 'accuracy'] = np.inf
     return df
