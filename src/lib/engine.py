9d307b4b2768556b13b8899581060b1fd41d9cad
brettmt10 <brettmt10@gmail.com>
2025-10-16 17:15:01 -0400

    rename coerece function

--- src/lib/engine.py
+++ src/lib/engine.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def infer(df: pd.DataFrame):
+def flag_coerce(df: pd.DataFrame):
     o_na = df['accuracy'].isna()
     df['accuracy'] = pd.to_numeric(df['accuracy'], errors='coerce')
     df['inferred_coerce'] = df['accuracy'].isna() & ~o_na
