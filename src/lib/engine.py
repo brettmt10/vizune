8862281989bc7d0c42b6a93b9d63dcf58f5a2eda
brettmt10 <brettmt10@gmail.com>
2025-10-16 20:26:16 -0400

    col specific type decision

--- src/lib/engine.py
+++ src/lib/engine.py
@@ -6,7 +6,7 @@ def flag_coerce(df: pd.DataFrame, col):
     o_na = df[col].isna()
     df[col] = pd.to_numeric(df[col], errors='coerce')
     df['inferred_coerce'] = df[col].isna() & ~o_na
-    return df['inferred_coerce']
+    return df[[col, 'inferred_coerce']]
 
-def type_decision(df: pd.DataFrame, col):
-    num_coerce = df['inferred_coerce'].sum() / len(df)
+def type_decision(df: pd.DataFrame):
+    return 'float' if df['inferred_coerce'].sum() / len(df) < 0.3 else 'string'
