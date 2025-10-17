1fb5217427caf71d94521478ebf868d45353fab9
brettmt10 <brettmt10@gmail.com>
2025-10-16 20:16:29 -0400

    fix original df assignment overwrite. return only inferred coerce

--- src/lib/engine.py
+++ src/lib/engine.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 
 def flag_coerce(df: pd.DataFrame, col):
+    df = df.copy()
     o_na = df[col].isna()
     df[col] = pd.to_numeric(df[col], errors='coerce')
     df['inferred_coerce'] = df[col].isna() & ~o_na
-    df.loc[df['inferred_coerce'], col] = np.inf
-    return df
+    return df['inferred_coerce']
 
 def type_decision(df: pd.DataFrame, col):
     num_coerce = df['inferred_coerce'].sum() / len(df)
