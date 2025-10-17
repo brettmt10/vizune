f7e5e0a2bb75ff5400502eaf3665cf2a5224e546
brettmt10 <brettmt10@gmail.com>
2025-10-16 20:34:29 -0400

    assign coerced-based values

--- src/lib/engine.py
+++ src/lib/engine.py
@@ -10,3 +10,11 @@ def flag_coerce(df: pd.DataFrame, col):
 
 def type_decision(df: pd.DataFrame):
     return 'float' if df['inferred_coerce'].sum() / len(df) < 0.3 else 'string'
+
+def coerce_vals(df: pd.DataFrame, col, type_d):
+    if type_d == 'float':
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+    else:
+        df[col] = df[col].astype(str)
+    return df
+    
