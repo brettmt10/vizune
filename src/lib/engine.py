d9c4f575c746762090ce4d30e928ba4a6fdd6320
brettmt10 <brettmt10@gmail.com>
2025-10-17 13:35:13 -0400

    clearer df copy var

--- src/lib/engine.py
+++ src/lib/engine.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 
 def flag_coerce(df: pd.DataFrame, col):
-    df = df.copy()
-    o_na = df[col].isna()
-    df[col] = pd.to_numeric(df[col], errors='coerce')
-    df['inferred_coerce'] = df[col].isna() & ~o_na
-    return df[[col, 'inferred_coerce']]
+    n_df = df.copy()
+    o_na = n_df[col].isna()
+    n_df[col] = pd.to_numeric(n_df[col], errors='coerce')
+    n_df['inferred_coerce'] = n_df[col].isna() & ~o_na
+    return n_df[[col, 'inferred_coerce']]
 
 def type_decision(df: pd.DataFrame):
     return 'float' if df['inferred_coerce'].sum() / len(df) < 0.3 else 'string'
@@ -17,4 +17,3 @@ def coerce_vals(df: pd.DataFrame, col, type_d):
     else:
         df[col] = df[col].astype(str)
     return df
-    
