d1ea0c1a3e0400ba5b3fa4d23337859d2a5b3976
brettmt10 <brettmt10@gmail.com>
2025-10-19 15:38:19 -0400

    improve memory allocation

--- src/lib/engine.py
+++ src/lib/engine.py
@@ -1,25 +1,21 @@
 import pandas as pd
 
 class TypeInference:
-    def _flag_coerce(self, df, col):
-        n_df = df.copy()
-        o_na = n_df[col].isna()
-        n_df[col] = pd.to_numeric(n_df[col], errors='coerce')
-        n_df['_inferred_coerce'] = n_df[col].isna() & ~o_na
-        return n_df['_inferred_coerce']
+    def _flag_coerce(self, df_c: pd.Series):
+        o_na = df_c.isna()
+        ic = pd.to_numeric(df_c, errors='coerce')
+        return ic.isna() & ~o_na
 
     def _type_decision(self, df_ic: pd.Series):
         return 'float' if df_ic.sum() / len(df_ic) < 0.3 else 'string'
 
-    def _coerce_vals(self, df, col, type_d):
-        n_df = df.copy()
+    def _coerce_vals(self, df_c, type_d):
         if type_d == 'float':
-            n_df[col] = pd.to_numeric(n_df[col], errors='coerce')
-        else:
-            n_df[col] = n_df[col].astype(str)
-        return n_df 
+            return pd.to_numeric(df_c, errors='coerce')
+        return df_c.astype(str) 
     
     def infer(self, df: pd.DataFrame, col):
-        df_ic = self._flag_coerce(df, col)
+        df_c = df[col]
+        df_ic = self._flag_coerce(df_c)
         type_d = self._type_decision(df_ic)
-        return self._coerce_vals(df, col, type_d)
+        return self._coerce_vals(df_c, type_d)
