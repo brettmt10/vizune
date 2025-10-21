cd979eabccc1a7734d27fb357f85d0e2fe919805
brettmt10 <brettmt10@gmail.com>
2025-10-20 21:05:21 -0400

    add BaseModel Union for valid chart responses

--- src/lib/responses.py
+++ src/lib/responses.py
@@ -1,11 +1,22 @@
 from pydantic import BaseModel
-from enum import Enum
-from typing import Literal
+from typing import Literal, Union
 
 class ResponseModelVizuneBar(BaseModel):
     chart: Literal['bar']
     x_lab: str
     y_lab: str
+    
+class ResponseModelVizuneLine(BaseModel):
+    chart: Literal['line']
+    x_lab: str
+    y_lab: str
+    smooth: bool
+    
+ChartResponseOptions = Union[
+    ResponseModelVizuneBar,
+    ResponseModelVizuneLine
+]
+
 
     
     
