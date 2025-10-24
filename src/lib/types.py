012af9a6f1d070444d2b1fd7be8fe8601a6fe6b2
brettmt10 <brettmt10@gmail.com>
2025-10-24 16:42:34 -0400

    support date value extraction

--- src/lib/types.py
+++ src/lib/types.py
@@ -8,23 +8,30 @@ class TypeInference:
     
     def _flag_coerce_dtm(self, df_c: pd.Series):
         o_na = df_c.isna()
-        ic = pd.to_datetime(df_c, errors='coerce')
+        ic = pd.to_datetime(df_c, errors='coerce', format='mixed')
         return ic.isna() & ~o_na
 
-    def _type_decision(self, df_ic: pd.Series):
-        return 'float' if df_ic.sum() / len(df_ic) < 0.3 else 'string'
+    def _type_decision(self, df_ics: pd.DataFrame):
+        # prioritize floats
+        if df_ics['ic_float'].sum() / len(df_ics['ic_float']) < 0.3: return 'float'
+        elif df_ics['ic_dtm'].sum() / len(df_ics['ic_dtm']) < 0.3: return 'datetime'
+        else: return 'string'
 
     def _coerce_vals(self, df_c, type_d):
         if type_d == 'float':
             return pd.to_numeric(df_c, errors='coerce')
-        return df_c.astype(str) 
+        elif type_d == 'datetime':
+            return pd.to_datetime(df_c, errors='coerce', format='mixed')
+        return df_c.astype(str)
     
     def infer(self, df: pd.DataFrame):
         metadata = {}
+        df_ics = pd.DataFrame()
         for col in df.columns:
             df_c = df[col]
-            df_ic = self._flag_coerce(df_c)
-            type_d = self._type_decision(df_ic)
+            df_ics['ic_float'] = self._flag_coerce_float(df_c)
+            df_ics['ic_dtm'] = self._flag_coerce_dtm(df_c)
+            type_d = self._type_decision(df_ics)
             df[col] = self._coerce_vals(df_c, type_d)
             metadata[col] = type_d
         return df, metadata
